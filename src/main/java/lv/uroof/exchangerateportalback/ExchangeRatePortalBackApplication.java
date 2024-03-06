ba1fd6c44d825f20caceadbd7416f3412f7b0ccc
Viktorija Gluhiha <viktorija.gluhiha@gmail.com>
2024-03-06 16:20:37 +0200

    Initialization of project

---
+++ src/main/java/lv/uroof/exchangerateportalback/ExchangeRatePortalBackApplication.java
@@ -0,0 +1,13 @@
+package lv.uroof.exchangerateportalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatePortalBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRatePortalBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lv/uroof/exchangerateportalback/ExchangeRatePortalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.uroof.exchangerateportalback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatePortalBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
