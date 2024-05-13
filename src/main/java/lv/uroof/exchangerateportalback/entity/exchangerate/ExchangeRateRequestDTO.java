fd37e5d3db35f8982162f8192a5a58b4136fb8b2
Viktorija Gluhiha <viktorija.gluhiha@gmail.com>
2024-05-14 02:58:53 +0300

    Removed useless field from ExchangeRate request DTO

--- src/main/java/lv/uroof/exchangerateportalback/entity/exchangerate/ExchangeRateRequestDTO.java
+++ src/main/java/lv/uroof/exchangerateportalback/entity/exchangerate/ExchangeRateRequestDTO.java
@@ -1,6 +1,5 @@
 package lv.uroof.exchangerateportalback.entity.exchangerate;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,9 +7,6 @@ import java.time.LocalDate;
 
 @Data
 public class ExchangeRateRequestDTO {
-    @NotBlank
-    private String code;
-
     private LocalDate date;
     private String currencyBaseCode;
     private String currencyQuoteCode;
